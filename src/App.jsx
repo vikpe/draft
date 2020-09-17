65e3d1e6874b84d5f78116221c6bab814e001fa2
Viktor Persson <viktor.persson@arcsin.se>
2020-09-17 10:17:36 +0200

    Reformat code.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
