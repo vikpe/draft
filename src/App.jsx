c1343a563f7dee94b437a76ab7eeb0d40a66114a
Viktor Persson <viktor.persson@arcsin.se>
2020-09-19 18:39:37 +0200

    Add GetQuad logo.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import SampleBoard from "./SampleBoard";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} width={32} /> GetQuad Draft
+    <div className="app">
+      <header className="app-header">
+
       </header>
 
       <section>
