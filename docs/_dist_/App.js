308a9833852b7c93e4156b157e4e14748dfd6876
Viktor Persson <viktor.persson@arcsin.se>
2020-09-19 18:39:57 +0200

    Rebuild.

--- docs/_dist_/App.js
+++ docs/_dist_/App.js
@@ -1,17 +1,13 @@
 import React from "../web_modules/react.js";
-import logo from "./logo.svg.proxy.js";
 import "./App.css.proxy.js";
 import SampleBoard from "./SampleBoard.js";
 
 function App() {
   return /*#__PURE__*/React.createElement("div", {
-    className: "App"
+    className: "app"
   }, /*#__PURE__*/React.createElement("header", {
-    className: "App-header"
-  }, /*#__PURE__*/React.createElement("img", {
-    src: logo,
-    width: 32
-  }), " GetQuad Draft"), /*#__PURE__*/React.createElement("section", null, /*#__PURE__*/React.createElement(SampleBoard, null)));
+    className: "app-header"
+  }), /*#__PURE__*/React.createElement("section", null, /*#__PURE__*/React.createElement(SampleBoard, null)));
 }
 
 export default App;
