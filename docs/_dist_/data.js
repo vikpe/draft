ea3bf2970d2e97d3f86d15ab2dc3e17fe2c92959
Viktor Persson <vikpe@users.noreply.github.com>
2021-10-02 16:50:14 +0200

    Update players.

--- docs/_dist_/data.js
+++ docs/_dist_/data.js
@@ -29,58 +29,54 @@ export const teams = {
   Spawn: {
     id: "Spawn",
     name: "Spawn",
-    playerNames: ["overflow/ovr"],
+    playerNames: ["rghst"],
     theme: "Spawn",
     sortOrder: 7
   },
   Shambler: {
     id: "Shambler",
     name: "Shambler",
-    playerNames: ["maks"],
+    playerNames: ["Rotker"],
     theme: "Shambler",
     sortOrder: 6
   },
   Fiend: {
     id: "Fiend",
     name: "Fiend",
-    playerNames: ["Sniegov"],
+    playerNames: ["ncr"],
     theme: "Fiend",
     sortOrder: 5
   },
   Zombie: {
     id: "Zombie",
     name: "Zombie",
-    playerNames: ["xero"],
+    playerNames: ["[o]djevulsk"],
     theme: "Zombie",
     sortOrder: 4
   },
   Gombie: {
     id: "Gombie",
     name: "Gombie",
-    playerNames: ["samon"],
+    playerNames: ["ocoini"],
     theme: "Gombie",
     sortOrder: 3
   },
   Enforcer: {
     id: "Enforcer",
     name: "Enforcer",
-    playerNames: ["kip"],
+    playerNames: ["Hooraytio"],
     theme: "Enforcer",
     sortOrder: 2
   },
   Rotfish: {
     id: "Rotfish",
     name: "Rotfish",
-    playerNames: ["Link"],
+    playerNames: ["TiMMi"],
     theme: "Rotfish",
     sortOrder: 1
   }
 };
 export const players = {
-  "6. Vegeta": {
-    name: "6. Vegeta",
-    country_code: "US"
-  },
   Abraxas: {
     name: "Abraxas",
     country_code: "NO"
@@ -96,10 +92,6 @@ export const players = {
   coj: {
     name: "coj",
     country_code: "US"
-  },
-  conan: {
-    name: "conan",
-    country_code: "SE"
   },
   ".devil": {
     name: ".devil",
@@ -121,22 +113,6 @@ export const players = {
     name: "dobeZz",
     country_code: "GB"
   },
-  Dopeskillz: {
-    name: "Dopeskillz",
-    country_code: "DE"
-  },
-  Effie: {
-    name: "Effie",
-    country_code: "NO"
-  },
-  Eikooren: {
-    name: "Eikooren",
-    country_code: "SE"
-  },
-  Elguapo: {
-    name: "Elguapo",
-    country_code: "SE"
-  },
   eppe: {
     name: "eppe",
     country_code: "SE"
@@ -144,18 +120,6 @@ export const players = {
   FinalExit: {
     name: "FinalExit",
     country_code: "MT"
-  },
-  Flamer: {
-    name: "Flamer",
-    country_code: "CZ"
-  },
-  Goniec: {
-    name: "Goniec",
-    country_code: "PL"
-  },
-  Grl: {
-    name: "Grl",
-    country_code: "PL"
   },
   HaraldQuake: {
     name: "HaraldQuake",
@@ -169,33 +133,13 @@ export const players = {
     name: "hyrveli",
     country_code: "FI"
   },
-  kip: {
-    name: "kip",
-    country_code: "FI"
-  },
   Klice: {
     name: "Klice",
     country_code: "FR"
   },
-  knasty: {
-    name: "knasty",
-    country_code: "DE"
-  },
-  Link: {
-    name: "Link",
-    country_code: "NO"
-  },
   Luckyshot: {
     name: "Luckyshot",
     country_code: "FI"
-  },
-  mads: {
-    name: "mads",
-    country_code: "RU"
-  },
-  maks: {
-    name: "maks",
-    country_code: "PL"
   },
   MolkyPolky: {
     name: "MolkyPolky",
@@ -221,14 +165,6 @@ export const players = {
     name: "Nico",
     country_code: "US"
   },
-  Nidweyr: {
-    name: "Nidweyr",
-    country_code: "UA"
-  },
-  nitemare: {
-    name: "nitemare",
-    country_code: "PT"
-  },
   ocoini: {
     name: "ocoini",
     country_code: "NO"
@@ -237,14 +173,6 @@ export const players = {
     name: "omicron",
     country_code: "US"
   },
-  "overflow/ovr": {
-    name: "overflow/ovr",
-    country_code: "CH"
-  },
-  paniagua: {
-    name: "paniagua",
-    country_code: "FI"
-  },
   Panjabi: {
     name: "Panjabi",
     country_code: "FI"
@@ -252,14 +180,6 @@ export const players = {
   pattah: {
     name: "pattah",
     country_code: "DE"
-  },
-  pooll: {
-    name: "pooll",
-    country_code: "PL"
-  },
-  PreMorteM: {
-    name: "PreMorteM",
-    country_code: "NO"
   },
   Respa: {
     name: "Respa",
@@ -277,33 +197,9 @@ export const players = {
     name: "Rotker",
     country_code: "PL"
   },
-  samon: {
-    name: "samon",
-    country_code: "PL"
-  },
-  skurk: {
-    name: "skurk",
-    country_code: "SE"
-  },
-  Sniegov: {
-    name: "Sniegov",
-    country_code: "PL"
-  },
-  splif: {
-    name: "splif",
-    country_code: "GB"
-  },
   STALKERRH: {
     name: "STALKERRH",
     country_code: "NO"
-  },
-  TheChosenOne: {
-    name: "TheChosenOne",
-    country_code: "NO"
-  },
-  Thunder: {
-    name: "Thunder",
-    country_code: "PL"
   },
   TiMMi: {
     name: "TiMMi",
@@ -317,10 +213,6 @@ export const players = {
     name: "tr0ll",
     country_code: "NO"
   },
-  tumult: {
-    name: "tumult",
-    country_code: "SE"
-  },
   Turbo: {
     name: "Turbo",
     country_code: "GB"
@@ -329,17 +221,9 @@ export const players = {
     name: "Valde",
     country_code: "DK"
   },
-  xero: {
-    name: "xero",
-    country_code: "SE"
-  },
   Ysbaddaden: {
     name: "Ysbaddaden",
     country_code: "RU"
-  },
-  Zalon: {
-    name: "Zalon",
-    country_code: "DK"
   },
   zigg: {
     name: "zigg",
