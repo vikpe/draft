d75ac059d71c06073e9b40583ec7dc038a265db4
Viktor Persson <viktor.persson@arcsin.se>
2022-09-17 20:49:15 +0200

    Update config.

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: "https://vikpe.org/draft",
+  base: "https://vikpe.org/draft/",
   plugins: [react()]
 })
