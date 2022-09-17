f03e40ef845c75d5237f08bb8e6ee7c93a10a29f
Viktor Persson <viktor.persson@arcsin.se>
2022-09-17 16:28:35 +0200

    Remove build from repo.

--- docs/_snowpack/pkg/react-dom.js
+++
@@ -1,3 +0,0 @@
-import { r as reactDom } from './common/index-765aa0d3.js';
-export { r as default } from './common/index-765aa0d3.js';
-import './common/index-370e7390.js';
--- docs/_snowpack/pkg/react.js
+++
@@ -1,2 +0,0 @@
-import { r as react } from './common/index-370e7390.js';
-export { r as default } from './common/index-370e7390.js';
